c531648c2d9371ab62ddeb40bb6df551f1320798
Aronno-arnob <101754921+Aronno-arnob@users.noreply.github.com>
2022-06-24 01:24:30 +0600

    Add files via upload

---
+++ Table.sql
@@ -0,0 +1,14 @@
+create database mydb;
+use mydb;
+Create table branch (branch_name varchar(20), branch_city varchar(20), assets integer);
+insert into branch(branch_name, branch_city, assets) values("Brighton", "Brooklyn", 7100000),("Downtown", "Brooklyn", 9000000),("Mianus", "Horseneck", 3700000),("NorthTown", "Rye", 400000),("Perryridge", "Horseneck", 1700000),("Pownal", "Bennington", 300000),("Redwood", "Palo Alto", 2100000),("Round Hill", "Horseneck", 800000);
+create table customer(customer_name varchar(20), customer_street varchar(20), customer_city varchar(20));
+insert into customer(customer_name, customer_street, customer_city) values("Adams", "Spring","Pittsfield"),("Brooks", "Senator","Brooklyn"),("Curry", "North","Rye"),("Glenn", "Sand Hill","Woodside"),("Green", "Walnut","stamford"),("Hayes", "Main","Harrison"),("Johnson", "Alma","Palo Alto"),("Jones", "Main","Harrison"),("Lindsay", "Park","Pittsfield"),("Turner", "Putnam","Stamford"),("Smith", "North","Rye"),("Williams", "Nassau","Princeton");
+create table loan(loan_number varchar(20) primary key, branch_name varchar(20), amount integer);
+insert into loan(loan_number, branch_name, amount) values("L-11", "Round Hill", 900),("L-14", "Downtown", 1500), ("L-15", "Perryridge",1500),("L-16", "Perryridge", 1300),("L-17", "Downtown", 1000), ("L-23", "Redwood", 2000), ("L-93", "Mianus", 500);
+create table borrower(customer_name varchar(20), loan_number varchar(20), foreign key (loan_number) References loan(loan_number));
+insert into borrower(customer_name, loan_number) values("Adams", "L-16"), ("Curry", "L-93"), ("Hayes", "L-15"), ("Jackson", "L-14"), ("Jones", "L-17"), ("Smith", "L-11"), ("Smith", "L-23"), ("Williams", "L-17");
+create table account(account_number varchar(20), branch_name varchar(20), balance integer, CONSTRAINT PK_account PRIMARY KEY (account_number));
+insert into account(account_number, branch_name, balance) values("A-101", "Downtown", 500),("A-102", "Perryridge", 400),("A-201", "Brighton", 900),("A-215", "Mianus", 700),("A-217", "Brighton", 750),("A-222", "Redwood", 700),("A-305", "Round Hill", 350);
+create table depositor (customer_name varchar(20), account_number varchar(20), foreign key (account_number) references account(account_number));
+insert into depositor (customer_name, account_number) values("Hayes", "A-102"),("Johnson", "A-101"),("Johnson", "A-201"),("Jones", "A-217"),("Lindsay", "A-222"),("Smith", "A-215"),("Turner", "A-305");
